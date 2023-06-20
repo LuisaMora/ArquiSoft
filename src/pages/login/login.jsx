9f69db312feeb8c003da1f94824c2f74756e4c0b
marcoescalera <82482012+marcoescalera@users.noreply.github.com>
2023-06-20 11:12:16 -0400

    Labels en espanol

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,22 +3,22 @@ export default function UpdateCategoriesPage() {
     return (
         <div className={styles.container}>
             <div className={styles.form}>
-            <h1 className={styles.titulo}>LOGIN</h1>
+            <h1 className={styles.titulo}>BIENVENIDO</h1>
             <form className={styles.formulario} action="/send-data-here" method="post">       
 
-                <label className={styles.labels}>Email</label><br></br>
+                <label className={styles.labels}>Correo electronico</label><br></br>
                 <input className={styles.captura} type="text" /><br></br>
 
-                <label className={styles.labels}>Password</label><br></br>
+                <label className={styles.labels}>Contraseña</label><br></br>
                 <input className={styles.captura}type="password"/><br></br>
 
                 <div className={styles.forgotContainer}>
-                    <a href='#' className={styles.forgot}>Has olvidado tu contrasena?</a><br></br>
+                    <a href='#' className={styles.forgot}>¿Has olvidado tu contraseña?</a><br></br>
                 </div>
                 
                 <div className={styles.posicionBoton}>
-                    <button className={styles.botonLI} type="submit">LOG IN</button><br></br>
-                    <button className={styles.botonSI} type="submit">SIGN IN</button><br></br>
+                    <button className={styles.botonLI} type="submit">INGRESAR</button><br></br>
+                    <button className={styles.botonSI} type="submit">REGISTRAR</button><br></br>
                 </div>
             </form>
             </div>
